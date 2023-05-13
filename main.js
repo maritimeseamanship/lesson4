65210adf429c166fa5c0a6c6a4bfab56a063f151
maritimeseamanship <ekaterinataer@gmail.com>
2023-05-13 20:54:33 +0300

    Second commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function area(a, b){
     let areaCalculation= a * b;
     console.log('The area of the rectangle is:', areaCalculation.toFixed(2));
+    return areaCalculation.toFixed(2);
 }
 
 let a = +prompt('Please enter the length of the rectangle');
@@ -14,8 +15,7 @@ if (a <= 0 || b <= 0){
 
 /*- створити функцію яка обчислює та повертає площу кола з радіусом r */
 function circleArea (r){
-    const pi = 3.14;
-    let circleCalculation = pi * Math.pow(r, 2);
+    let circleCalculation = Math.PI * Math.pow(r, 2);
     console.log('The area of the circle is:', circleCalculation.toFixed(2));
     return circleCalculation.toFixed(2);
 }
@@ -29,8 +29,7 @@ if (r <= 0 ){
 
 /*- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r*/
 function cylinderArea(rc, h){
-    const pi = 3.14;
-    let cylinderCalculation = 2 * pi * rc * h;
+    let cylinderCalculation = 2 * Math.PI * rc * h;
     console.log('The area of the cylinder is:', cylinderCalculation.toFixed(2));
     return cylinderCalculation.toFixed(2);
 }
@@ -96,10 +95,65 @@ function arrToList(array){
 
 let array = [8, 9, 10];
 arrToList(array);
-//- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-//- створити функцію яка повертає найменьше число з масиву
-//- створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-/*- створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+
+
+/*- створити функцію яка приймає масив об'єктів з такими полями: id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок*/
+const arrUsers = [
+    {id: 1, name: 'Julia', age: 32},
+    {id: 2, name: 'Maria', age: 30},
+    {id: 3, name: 'Mark', age: 38}
+]
+
+function usersPrinter(arr){
+    for(const arrElement of arr){
+        document.write(`<div>`);
+        document.write(`id: ${arrElement.id}, name: ${arrElement.name}, age: ${arrElement.age}`);
+        document.write(`</div>`);
+    }
+}
+usersPrinter(arrUsers);
+/*- створити функцію яка повертає найменьше число з масиву */
+function minNumber (arr) {
+    let minNum = arr[0];
+    let i = 0;
+    while (i < arr.length) {
+        if (arr[i] < minNum) {
+            minNum = arr[i];
+            i++;
+        }
+    } return minNum;
+}
+
+let arrNum = [1, 2, 3, 4, -56, 0];
+console.log('The minimal number is: ', minNumber(arrNum));
+
+/*- створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) ->13*/
+function sum (arr){
+    let sumOfElements = 0;
+    for (let arrElement of arr){
+        sumOfElements = sumOfElements + arrElement;
+    }
+    return sumOfElements;
+}
+console.log('The sum of all elements: ', sum ( [6,8,10,5]));
+
+/*- створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
 Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]*/
+function swap (arr, index1, index2){
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+  return arr;
+}
+console.log('The result is: ',  swap([11,22,33,44],0,1));
+
 /*- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250*/
+function exchange(sumUAH, currencyValues, exchangeCurrency){
+    for (const item of currencyValues){
+        if (item.currency === exchangeCurrency){
+           return sumUAH/item.value;
+        }
+    }
+}
+console.log(exchange(10000,
+                     [{currency:'USD',value:40},{currency:'EUR',value:42}],
+                     'USD'));
